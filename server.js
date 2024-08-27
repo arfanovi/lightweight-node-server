796c5712d9ee7065ceb6409bf72cc7c487e01794
arfanovi <arfanovi.gub1@gmail.com>
2024-08-27 17:11:38 +0600

    Route handler

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ const sendResponse = (res, content, statusCode = 200) => {
     res.end(content)
 }
 
+
+
+// Route Handler 
+const handleHome = (res) => sendResponse(res, 'This is the Home Page');
+const handleAbout = (res) => sendResponse(res, 'This is the About Page');
+const handleContact = (res) => sendResponse(res, 'This is the Contact Page');
+
+const handleFileWrite = (res) => {
+    fs.writeFile(path.join(__dirname, 'demo.txt'), 'Hello Server with File Write', (err) => {
+        sendResponse(res, err ? 'Error writing file' : 'File has been written ', err ? 500 : 200)
+    })
+}
+
